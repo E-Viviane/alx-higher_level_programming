28abeb8c26505620604e908e5f400a7866546651
E-Viviane <vewedje@gmail.com>
2022-08-23 14:38:48 -0700

    programming python

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+ist_digit(number):
+
+        if number < 0:
+
+                    number = number * -1
+
+                        number = number % 10
+
+                            print("{}".format(number), end='')
+
+                                return number
