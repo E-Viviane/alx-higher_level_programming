721165c8b0e2ce384a2735c52d3b9ac447d0de7f
E-Viviane <vewedje@gmail.com>
2022-09-26 13:22:50 -0700

    programming

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+
+okup.py
+
+"""Defines an object attribute lookup function."""
+
+
+
+
+
+def lookup(obj):
+
+        """Return a list of an object's available attributes."""
+
+            return (dir(obj))
