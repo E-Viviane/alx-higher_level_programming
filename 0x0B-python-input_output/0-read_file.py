bbf8dc721ce234577239c100b4ffd28a7e988a7b
E-Viviane <104867990+E-Viviane@users.noreply.github.com>
2022-09-28 20:46:58 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-'''A module containing IO functions.
-'''
+# 0-read_file.py
+"""Defines a text file-reading function."""
 
 
-def write_file(filename="", text=""):
-    '''Writes a UTF-8 encoded text to a file.
-    Args:
-        filename (str): The name of the file to write to.
-        text (str): The content to store in the file.
-    Returns:
-        int: The number of characters written.
-    '''
-    n = 0
-    with open(filename, mode='w', encoding='utf-8') as file:
-        n = file.write(text)
-    return n
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
